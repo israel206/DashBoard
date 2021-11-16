f823ef17e9bbe9a381da4ad48193b3164093e7f5
israel <israel.pereira@dcx.ufpb.br>
2021-11-16 01:09:27 -0300

    modificações nos file charts e index

--- js/charts.js
+++ js/charts.js
@@ -1,5 +1,7 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var earning = document.getElementById('earning').getContext('2d');
+var radar = document.getElementById('radar').getContext('2d');
+var growth = document.getElementById('growth').getContext('2d');
 
 var myChart = new Chart(ctx, {
   type: 'polarArea',
@@ -26,6 +28,53 @@ var myChart = new Chart(earning, {
     labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
     datasets: [{
       label: 'Earning',
+      data: [4534, 1923, 3123, 5453, 2333, 3546, 3247, 4356, 3215, 5467, 3240, 7685],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+    }]
+  },
+  options: {
+    responsive: true,
+  }
+});
+
+var myChart = new Chart(radar, {
+  type: 'doughnut',
+  data: {
+    labels: ['Facebook', 'Youtube', 'Amazon'],
+    datasets: [{
+      label: 'Traffic Source',
+      data: [1250, 1980, 3040],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)'
+      ],
+    }]
+  },
+  options: {
+    responsive: true,
+  }
+});
+
+var myChart = new Chart(growth, {
+  type: 'line',
+  data: {
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    datasets: [{
+      label: 'growth',
       data: [4534, 1923, 3123, 5453, 2333, 3546, 3247, 4356, 3215, 5467, 3240, 7685],
       backgroundColor: [
         'rgba(255, 99, 132, 1)',
