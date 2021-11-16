5e2b00d63daf3256f05339d81335c3e574e73639
israel <israel.pereira@dcx.ufpb.br>
2021-11-16 00:16:14 -0300

    criando charts

--- README.md
+++ README.md
@@ -17,6 +17,7 @@
   <img src="https://img.shields.io/github/stars/israel206/DashBoard?label=stars&message=MIT&color=FFFFFF&labelColor=32B768" alt="Stars">
   <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=49AA26&labelColor=000000" alt="PRs welcome!" />
   <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+  
 </p>
 
 <br>
---
+++ charts.js
@@ -0,0 +1,49 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var earning = document.getElementById('myChart').getContext('2d');
+
+var myChart = new Chart(ctx, {
+  type: 'polarArea',
+  data: {
+    labels: ['Facebook', 'Youtube', 'Amazon'],
+    datasets: [{
+      label: 'Traffic Source',
+      data: [1250, 1980, 3040],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)'
+      ],
+    }]
+  },
+  options: {
+    responsive: true,
+  }
+});
+
+var myChart = new Chart(earning, {
+  type: 'bar',
+  data: {
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    datasets: [{
+      label: 'Earning',
+      data: [4534, 1923, 3123, 5453, 2333, 3546, 3247, 4356, 3215, 5467, 3240, 7685],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+    }]
+  },
+  options: {
+    responsive: true,
+  }
+});
